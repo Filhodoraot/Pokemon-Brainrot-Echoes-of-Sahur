1492868e30b2b1818ad49c37172c4ab6f40749d8
Filhodoraot <deoliveiraenzo.vieira@gmail.com>
2026-07-07 18:03:27 -0300

    Auto-detect devkitARM in Windows Brainrot build helper

--- tools/build_brainrot_windows_modern.bat
+++ tools/build_brainrot_windows_modern.bat
@@ -9,7 +9,42 @@ if errorlevel 1 goto no_python
 where make >nul 2>nul
 if errorlevel 1 goto no_make
 
+rem Try common devkitPro paths first.
+if exist "C:\devkitPro\devkitARM\bin\arm-none-eabi-gcc.exe" (
+    set "DEVKITPRO=C:\devkitPro"
+    set "DEVKITARM=C:\devkitPro\devkitARM"
+    set "PATH=C:\devkitPro\devkitARM\bin;%PATH%"
+)
+
+if exist "C:\devkitPro\msys2\opt\devkitpro\devkitARM\bin\arm-none-eabi-gcc.exe" (
+    set "DEVKITPRO=C:\devkitPro\msys2\opt\devkitpro"
+    set "DEVKITARM=C:\devkitPro\msys2\opt\devkitpro\devkitARM"
+    set "PATH=C:\devkitPro\msys2\opt\devkitpro\devkitARM\bin;%PATH%"
+)
+
+if exist "C:\msys64\opt\devkitpro\devkitARM\bin\arm-none-eabi-gcc.exe" (
+    set "DEVKITPRO=C:\msys64\opt\devkitpro"
+    set "DEVKITARM=C:\msys64\opt\devkitpro\devkitARM"
+    set "PATH=C:\msys64\opt\devkitpro\devkitARM\bin;%PATH%"
+)
+
+rem If PowerShell/devkitPro added pacman but not compiler PATH, search a little deeper.
 where arm-none-eabi-gcc >nul 2>nul
+if errorlevel 1 (
+    for /f "delims=" %%G in ('dir /b /s "C:\arm-none-eabi-gcc.exe" "C:\devkitPro\arm-none-eabi-gcc.exe" "C:\msys64\arm-none-eabi-gcc.exe" 2^>nul') do (
+        set "GCC_PATH=%%G"
+        goto found_gcc
+    )
+    goto no_arm
+)
+goto have_gcc
+
+:found_gcc
+for %%D in ("%GCC_PATH%") do set "GCC_BIN=%%~dpD"
+set "PATH=%GCC_BIN%;%PATH%"
+
+:have_gcc
+arm-none-eabi-gcc --version
 if errorlevel 1 goto no_arm
 
 python tools\apply_brainrot_test_build.py
@@ -36,7 +71,9 @@ pause
 goto end
 
 :no_arm
-echo arm-none-eabi-gcc was not found. Install devkitPro/devkitARM and open this from its MSYS2/devkitPro shell.
+echo arm-none-eabi-gcc was not found in PATH or common devkitPro folders.
+echo Try this in PowerShell:
+echo Get-ChildItem C:\ -Filter arm-none-eabi-gcc.exe -Recurse -ErrorAction SilentlyContinue ^| Select-Object -First 5 FullName
 pause
 goto end
 
